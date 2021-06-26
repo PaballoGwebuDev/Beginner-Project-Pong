b127a69839cefe5216bbad02c37307f64a1f1a0e
PaballoGwebuDev <85556042+PaballoGwebuDev@users.noreply.github.com>
2021-06-26 17:59:51 +0200

    Load System commit
    -Added a Title screen
    -Added a GameManager that:
    persists throughout the game
    implements scene management

---
+++ Assets/Scripts/UI/BootLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootLoader : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameManager.Instance.LoadLevel(1);
+        }
+ 
+    }
+
+}
